7ed8bb2360985753ec9f0b9d48e55b26159bc230
Andy Stef <andy.stef.2512@gmail.com>
2020-09-14 13:42:58 +0300

    New ver

---
+++ StefFrame.framework/Headers/StefFrame.h
@@ -0,0 +1,19 @@
+//
+//  StefFrame.h
+//  StefFrame
+//
+//  Created by Andy Stef on 14.09.2020.
+//  Copyright © 2020 Andy Stef. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for StefFrame.
+FOUNDATION_EXPORT double StefFrameVersionNumber;
+
+//! Project version string for StefFrame.
+FOUNDATION_EXPORT const unsigned char StefFrameVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StefFrame/PublicHeader.h>
+
+
